a56ec6114f6ab6f75a723794cfcbc8d53d6f12df
jakub-fabia <jakub.fabia@proton.me>
2025-01-25 00:42:06 +0100

    Database models

---
+++ backend/models/order.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    address: {
+        city: {
+            type: String,
+            required: true,
+        },
+        country: String,
+        state: String,
+        zipcode: String,
+    },
+    phone: {
+        type: Number,
+        required: true,
+    },
+    products: [
+        {
+            bookId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1,
+            },
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        required: true,
+    },
+    orderStatus: {type: String}
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model('Order', orderSchema);
